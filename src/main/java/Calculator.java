67996f1a2c8da23664d5152045f35392274b79f5
haleyw49 <haleywiese49@gmail.com>
2022-10-12 10:57:54 -0600

    fixed int to binary

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -31,12 +31,8 @@ class Calculator {
 
 
     String intToBinaryNumber(int n){
-        StringBuilder result = new StringBuilder();
-        for (int i = 31; i >= 0; i--) {
-            int mask = 1 << i;
-            result.append((n & mask) != 0 ? "1" : "0");
-        }
-        return result.toString();
+        String answer = Integer.toBinaryString(n);
+        return answer;
     }
 
     /*
